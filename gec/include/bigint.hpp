d461a5f906d9315e900bdf7a03ca4467e092d1b6
Adz2323 <146026081+Adz2323@users.noreply.github.com>
2025-01-13 08:58:26 +0000

    bigint.hpp

---
+++ gec/include/bigint.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef GEC_BIGINT_HPP
+#define GEC_BIGINT_HPP
+
+#include "bigint/data.hpp"
+#include "bigint/macros.hpp"
+#include "bigint/mixin.hpp"
+#include "bigint/preset.hpp"
+
+#endif // !GEC_BIGINT_HPP
